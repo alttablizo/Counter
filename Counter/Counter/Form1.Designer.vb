c14f267a076a08cb7e6a4e152383a821d71dffb0
almrtblz <47944509+alttablizo@users.noreply.github.com>
2020-02-29 14:13:48 +0800

    Add files via upload

---
+++ Counter/Counter/Form1.Designer.vb
@@ -0,0 +1,420 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.led1 = New System.Windows.Forms.Button()
+        Me.led2 = New System.Windows.Forms.Button()
+        Me.led3 = New System.Windows.Forms.Button()
+        Me.led4 = New System.Windows.Forms.Button()
+        Me.led5 = New System.Windows.Forms.Button()
+        Me.led9 = New System.Windows.Forms.Button()
+        Me.led10 = New System.Windows.Forms.Button()
+        Me.led11 = New System.Windows.Forms.Button()
+        Me.led12 = New System.Windows.Forms.Button()
+        Me.led13 = New System.Windows.Forms.Button()
+        Me.led14 = New System.Windows.Forms.Button()
+        Me.led15 = New System.Windows.Forms.Button()
+        Me.led20 = New System.Windows.Forms.Button()
+        Me.led22 = New System.Windows.Forms.Button()
+        Me.led23 = New System.Windows.Forms.Button()
+        Me.led24 = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.contbuttn = New System.Windows.Forms.Button()
+        Me.stopbutton = New System.Windows.Forms.Button()
+        Me.led16 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.led21 = New System.Windows.Forms.Button()
+        Me.led25 = New System.Windows.Forms.Button()
+        Me.led19 = New System.Windows.Forms.Button()
+        Me.led18 = New System.Windows.Forms.Button()
+        Me.led17 = New System.Windows.Forms.Button()
+        Me.led8 = New System.Windows.Forms.Button()
+        Me.led6 = New System.Windows.Forms.Button()
+        Me.led7 = New System.Windows.Forms.Button()
+        Me.Timer = New System.Windows.Forms.Timer(Me.components)
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'led1
+        '
+        Me.led1.BackColor = System.Drawing.Color.LightGray
+        Me.led1.ForeColor = System.Drawing.Color.Silver
+        Me.led1.Location = New System.Drawing.Point(34, 34)
+        Me.led1.Name = "led1"
+        Me.led1.Size = New System.Drawing.Size(30, 30)
+        Me.led1.TabIndex = 0
+        Me.led1.UseVisualStyleBackColor = False
+        '
+        'led2
+        '
+        Me.led2.BackColor = System.Drawing.Color.LightGray
+        Me.led2.ForeColor = System.Drawing.Color.Silver
+        Me.led2.Location = New System.Drawing.Point(69, 34)
+        Me.led2.Name = "led2"
+        Me.led2.Size = New System.Drawing.Size(30, 30)
+        Me.led2.TabIndex = 0
+        Me.led2.UseVisualStyleBackColor = False
+        '
+        'led3
+        '
+        Me.led3.BackColor = System.Drawing.Color.LightGray
+        Me.led3.ForeColor = System.Drawing.Color.Silver
+        Me.led3.Location = New System.Drawing.Point(105, 34)
+        Me.led3.Name = "led3"
+        Me.led3.Size = New System.Drawing.Size(30, 30)
+        Me.led3.TabIndex = 0
+        Me.led3.UseVisualStyleBackColor = False
+        '
+        'led4
+        '
+        Me.led4.BackColor = System.Drawing.Color.LightGray
+        Me.led4.ForeColor = System.Drawing.Color.Silver
+        Me.led4.Location = New System.Drawing.Point(141, 34)
+        Me.led4.Name = "led4"
+        Me.led4.Size = New System.Drawing.Size(30, 30)
+        Me.led4.TabIndex = 0
+        Me.led4.UseVisualStyleBackColor = False
+        '
+        'led5
+        '
+        Me.led5.BackColor = System.Drawing.Color.LightGray
+        Me.led5.ForeColor = System.Drawing.Color.Silver
+        Me.led5.Location = New System.Drawing.Point(177, 34)
+        Me.led5.Name = "led5"
+        Me.led5.Size = New System.Drawing.Size(30, 30)
+        Me.led5.TabIndex = 0
+        Me.led5.UseVisualStyleBackColor = False
+        '
+        'led9
+        '
+        Me.led9.BackColor = System.Drawing.Color.LightGray
+        Me.led9.ForeColor = System.Drawing.Color.Silver
+        Me.led9.Location = New System.Drawing.Point(141, 70)
+        Me.led9.Name = "led9"
+        Me.led9.Size = New System.Drawing.Size(30, 30)
+        Me.led9.TabIndex = 0
+        Me.led9.UseVisualStyleBackColor = False
+        '
+        'led10
+        '
+        Me.led10.BackColor = System.Drawing.Color.LightGray
+        Me.led10.ForeColor = System.Drawing.Color.Silver
+        Me.led10.Location = New System.Drawing.Point(177, 70)
+        Me.led10.Name = "led10"
+        Me.led10.Size = New System.Drawing.Size(30, 30)
+        Me.led10.TabIndex = 0
+        Me.led10.UseVisualStyleBackColor = False
+        '
+        'led11
+        '
+        Me.led11.BackColor = System.Drawing.Color.LightGray
+        Me.led11.ForeColor = System.Drawing.Color.Silver
+        Me.led11.Location = New System.Drawing.Point(33, 106)
+        Me.led11.Name = "led11"
+        Me.led11.Size = New System.Drawing.Size(30, 30)
+        Me.led11.TabIndex = 0
+        Me.led11.UseVisualStyleBackColor = False
+        '
+        'led12
+        '
+        Me.led12.BackColor = System.Drawing.Color.LightGray
+        Me.led12.ForeColor = System.Drawing.Color.Silver
+        Me.led12.Location = New System.Drawing.Point(69, 106)
+        Me.led12.Name = "led12"
+        Me.led12.Size = New System.Drawing.Size(30, 30)
+        Me.led12.TabIndex = 0
+        Me.led12.UseVisualStyleBackColor = False
+        '
+        'led13
+        '
+        Me.led13.BackColor = System.Drawing.Color.LightGray
+        Me.led13.ForeColor = System.Drawing.Color.Silver
+        Me.led13.Location = New System.Drawing.Point(105, 106)
+        Me.led13.Name = "led13"
+        Me.led13.Size = New System.Drawing.Size(30, 30)
+        Me.led13.TabIndex = 0
+        Me.led13.UseVisualStyleBackColor = False
+        '
+        'led14
+        '
+        Me.led14.BackColor = System.Drawing.Color.LightGray
+        Me.led14.ForeColor = System.Drawing.Color.Silver
+        Me.led14.Location = New System.Drawing.Point(141, 106)
+        Me.led14.Name = "led14"
+        Me.led14.Size = New System.Drawing.Size(30, 30)
+        Me.led14.TabIndex = 0
+        Me.led14.UseVisualStyleBackColor = False
+        '
+        'led15
+        '
+        Me.led15.BackColor = System.Drawing.Color.LightGray
+        Me.led15.ForeColor = System.Drawing.Color.Silver
+        Me.led15.Location = New System.Drawing.Point(177, 106)
+        Me.led15.Name = "led15"
+        Me.led15.Size = New System.Drawing.Size(30, 30)
+        Me.led15.TabIndex = 0
+        Me.led15.UseVisualStyleBackColor = False
+        '
+        'led20
+        '
+        Me.led20.BackColor = System.Drawing.Color.LightGray
+        Me.led20.ForeColor = System.Drawing.Color.Silver
+        Me.led20.Location = New System.Drawing.Point(177, 142)
+        Me.led20.Name = "led20"
+        Me.led20.Size = New System.Drawing.Size(30, 30)
+        Me.led20.TabIndex = 0
+        Me.led20.UseVisualStyleBackColor = False
+        '
+        'led22
+        '
+        Me.led22.BackColor = System.Drawing.Color.LightGray
+        Me.led22.ForeColor = System.Drawing.Color.Silver
+        Me.led22.Location = New System.Drawing.Point(69, 178)
+        Me.led22.Name = "led22"
+        Me.led22.Size = New System.Drawing.Size(30, 30)
+        Me.led22.TabIndex = 0
+        Me.led22.UseVisualStyleBackColor = False
+        '
+        'led23
+        '
+        Me.led23.BackColor = System.Drawing.Color.LightGray
+        Me.led23.ForeColor = System.Drawing.Color.Silver
+        Me.led23.Location = New System.Drawing.Point(105, 178)
+        Me.led23.Name = "led23"
+        Me.led23.Size = New System.Drawing.Size(30, 30)
+        Me.led23.TabIndex = 0
+        Me.led23.UseVisualStyleBackColor = False
+        '
+        'led24
+        '
+        Me.led24.BackColor = System.Drawing.Color.LightGray
+        Me.led24.ForeColor = System.Drawing.Color.Silver
+        Me.led24.Location = New System.Drawing.Point(141, 178)
+        Me.led24.Name = "led24"
+        Me.led24.Size = New System.Drawing.Size(30, 30)
+        Me.led24.TabIndex = 0
+        Me.led24.UseVisualStyleBackColor = False
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Panel1.Controls.Add(Me.contbuttn)
+        Me.Panel1.Controls.Add(Me.stopbutton)
+        Me.Panel1.Controls.Add(Me.led16)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.led21)
+        Me.Panel1.Controls.Add(Me.led25)
+        Me.Panel1.Controls.Add(Me.led24)
+        Me.Panel1.Controls.Add(Me.led19)
+        Me.Panel1.Controls.Add(Me.led20)
+        Me.Panel1.Controls.Add(Me.led15)
+        Me.Panel1.Controls.Add(Me.led14)
+        Me.Panel1.Controls.Add(Me.led23)
+        Me.Panel1.Controls.Add(Me.led10)
+        Me.Panel1.Controls.Add(Me.led9)
+        Me.Panel1.Controls.Add(Me.led13)
+        Me.Panel1.Controls.Add(Me.led22)
+        Me.Panel1.Controls.Add(Me.led5)
+        Me.Panel1.Controls.Add(Me.led12)
+        Me.Panel1.Controls.Add(Me.led4)
+        Me.Panel1.Controls.Add(Me.led18)
+        Me.Panel1.Controls.Add(Me.led17)
+        Me.Panel1.Controls.Add(Me.led11)
+        Me.Panel1.Controls.Add(Me.led3)
+        Me.Panel1.Controls.Add(Me.led8)
+        Me.Panel1.Controls.Add(Me.led6)
+        Me.Panel1.Controls.Add(Me.led7)
+        Me.Panel1.Controls.Add(Me.led2)
+        Me.Panel1.Controls.Add(Me.led1)
+        Me.Panel1.Location = New System.Drawing.Point(12, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(235, 263)
+        Me.Panel1.TabIndex = 1
+        '
+        'contbuttn
+        '
+        Me.contbuttn.BackColor = System.Drawing.Color.Green
+        Me.contbuttn.Location = New System.Drawing.Point(124, 232)
+        Me.contbuttn.Name = "contbuttn"
+        Me.contbuttn.Size = New System.Drawing.Size(25, 23)
+        Me.contbuttn.TabIndex = 3
+        Me.contbuttn.UseVisualStyleBackColor = False
+        '
+        'stopbutton
+        '
+        Me.stopbutton.BackColor = System.Drawing.Color.Red
+        Me.stopbutton.Location = New System.Drawing.Point(74, 232)
+        Me.stopbutton.Name = "stopbutton"
+        Me.stopbutton.Size = New System.Drawing.Size(25, 23)
+        Me.stopbutton.TabIndex = 3
+        Me.stopbutton.UseVisualStyleBackColor = False
+        '
+        'led16
+        '
+        Me.led16.BackColor = System.Drawing.Color.LightGray
+        Me.led16.ForeColor = System.Drawing.Color.Silver
+        Me.led16.Location = New System.Drawing.Point(34, 142)
+        Me.led16.Name = "led16"
+        Me.led16.Size = New System.Drawing.Size(30, 30)
+        Me.led16.TabIndex = 2
+        Me.led16.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.LightGray
+        Me.Button1.ForeColor = System.Drawing.Color.Silver
+        Me.Button1.Location = New System.Drawing.Point(33, 142)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(30, 30)
+        Me.Button1.TabIndex = 2
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'led21
+        '
+        Me.led21.BackColor = System.Drawing.Color.LightGray
+        Me.led21.ForeColor = System.Drawing.Color.Silver
+        Me.led21.Location = New System.Drawing.Point(34, 178)
+        Me.led21.Name = "led21"
+        Me.led21.Size = New System.Drawing.Size(30, 30)
+        Me.led21.TabIndex = 2
+        Me.led21.UseVisualStyleBackColor = False
+        '
+        'led25
+        '
+        Me.led25.BackColor = System.Drawing.Color.LightGray
+        Me.led25.ForeColor = System.Drawing.Color.Silver
+        Me.led25.Location = New System.Drawing.Point(177, 178)
+        Me.led25.Name = "led25"
+        Me.led25.Size = New System.Drawing.Size(30, 30)
+        Me.led25.TabIndex = 1
+        Me.led25.UseVisualStyleBackColor = False
+        '
+        'led19
+        '
+        Me.led19.BackColor = System.Drawing.Color.LightGray
+        Me.led19.ForeColor = System.Drawing.Color.Silver
+        Me.led19.Location = New System.Drawing.Point(141, 142)
+        Me.led19.Name = "led19"
+        Me.led19.Size = New System.Drawing.Size(30, 30)
+        Me.led19.TabIndex = 0
+        Me.led19.UseVisualStyleBackColor = False
+        '
+        'led18
+        '
+        Me.led18.BackColor = System.Drawing.Color.LightGray
+        Me.led18.ForeColor = System.Drawing.Color.Silver
+        Me.led18.Location = New System.Drawing.Point(105, 142)
+        Me.led18.Name = "led18"
+        Me.led18.Size = New System.Drawing.Size(30, 30)
+        Me.led18.TabIndex = 0
+        Me.led18.UseVisualStyleBackColor = False
+        '
+        'led17
+        '
+        Me.led17.BackColor = System.Drawing.Color.LightGray
+        Me.led17.ForeColor = System.Drawing.Color.Silver
+        Me.led17.Location = New System.Drawing.Point(69, 142)
+        Me.led17.Name = "led17"
+        Me.led17.Size = New System.Drawing.Size(30, 30)
+        Me.led17.TabIndex = 0
+        Me.led17.UseVisualStyleBackColor = False
+        '
+        'led8
+        '
+        Me.led8.BackColor = System.Drawing.Color.LightGray
+        Me.led8.ForeColor = System.Drawing.Color.Silver
+        Me.led8.Location = New System.Drawing.Point(105, 70)
+        Me.led8.Name = "led8"
+        Me.led8.Size = New System.Drawing.Size(30, 30)
+        Me.led8.TabIndex = 0
+        Me.led8.UseVisualStyleBackColor = False
+        '
+        'led6
+        '
+        Me.led6.BackColor = System.Drawing.Color.LightGray
+        Me.led6.ForeColor = System.Drawing.Color.Silver
+        Me.led6.Location = New System.Drawing.Point(34, 70)
+        Me.led6.Name = "led6"
+        Me.led6.Size = New System.Drawing.Size(30, 30)
+        Me.led6.TabIndex = 0
+        Me.led6.UseVisualStyleBackColor = False
+        '
+        'led7
+        '
+        Me.led7.BackColor = System.Drawing.Color.LightGray
+        Me.led7.ForeColor = System.Drawing.Color.Silver
+        Me.led7.Location = New System.Drawing.Point(69, 70)
+        Me.led7.Name = "led7"
+        Me.led7.Size = New System.Drawing.Size(30, 30)
+        Me.led7.TabIndex = 0
+        Me.led7.UseVisualStyleBackColor = False
+        '
+        'Timer
+        '
+        Me.Timer.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(262, 279)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents led1 As Button
+    Friend WithEvents led2 As Button
+    Friend WithEvents led3 As Button
+    Friend WithEvents led4 As Button
+    Friend WithEvents led5 As Button
+    Friend WithEvents led9 As Button
+    Friend WithEvents led10 As Button
+    Friend WithEvents led11 As Button
+    Friend WithEvents led12 As Button
+    Friend WithEvents led13 As Button
+    Friend WithEvents led14 As Button
+    Friend WithEvents led15 As Button
+    Friend WithEvents led20 As Button
+    Friend WithEvents led22 As Button
+    Friend WithEvents led23 As Button
+    Friend WithEvents led24 As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Timer As Timer
+    Friend WithEvents led21 As Button
+    Friend WithEvents led25 As Button
+    Friend WithEvents led19 As Button
+    Friend WithEvents led18 As Button
+    Friend WithEvents led17 As Button
+    Friend WithEvents led8 As Button
+    Friend WithEvents led7 As Button
+    Friend WithEvents led6 As Button
+    Friend WithEvents led16 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents stopbutton As Button
+    Friend WithEvents contbuttn As Button
+End Class
---
+++ Counter/Counter/Form1.vb
@@ -0,0 +1,258 @@
+﻿Public Class Form1
+
+    Public counter As Integer = -1
+    Private Sub offLights()
+        led1.BackColor = Color.LightGray
+        led2.BackColor = Color.LightGray
+        led3.BackColor = Color.LightGray
+        led4.BackColor = Color.LightGray
+        led5.BackColor = Color.LightGray
+        led6.BackColor = Color.LightGray
+        led7.BackColor = Color.LightGray
+        led8.BackColor = Color.LightGray
+        led9.BackColor = Color.LightGray
+        led10.BackColor = Color.LightGray
+        led11.BackColor = Color.LightGray
+        led12.BackColor = Color.LightGray
+        led13.BackColor = Color.LightGray
+        led14.BackColor = Color.LightGray
+        led15.BackColor = Color.LightGray
+        led16.BackColor = Color.LightGray
+        led17.BackColor = Color.LightGray
+        led18.BackColor = Color.LightGray
+        led19.BackColor = Color.LightGray
+        led20.BackColor = Color.LightGray
+        led21.BackColor = Color.LightGray
+        led22.BackColor = Color.LightGray
+        led23.BackColor = Color.LightGray
+        led24.BackColor = Color.LightGray
+        led25.BackColor = Color.LightGray
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer.Start()
+    End Sub
+
+    Private Sub Timer_Tick(sender As Object, e As EventArgs) Handles Timer.Tick
+        counter += 1
+
+        If counter > 9 Then
+            counter = 0
+        End If
+
+        If counter = 0 Then
+            offLights()
+            led2.BackColor = Color.Red
+            led3.BackColor = Color.Red
+            led4.BackColor = Color.Red
+            led6.BackColor = Color.Red
+            led7.BackColor = Color.Red
+            led9.BackColor = Color.Red
+            led10.BackColor = Color.Red
+            led11.BackColor = Color.Red
+            led12.BackColor = Color.Red
+            led14.BackColor = Color.Red
+            led15.BackColor = Color.Red
+            led16.BackColor = Color.Red
+            led17.BackColor = Color.Red
+            led19.BackColor = Color.Red
+            led20.BackColor = Color.Red
+            led22.BackColor = Color.Red
+            led23.BackColor = Color.Red
+            led24.BackColor = Color.Red
+
+        End If
+
+        If counter = 1 Then
+            offLights()
+            led3.BackColor = Color.Red
+            led4.BackColor = Color.Red
+            led7.BackColor = Color.Red
+            led8.BackColor = Color.Red
+            led9.BackColor = Color.Red
+            led13.BackColor = Color.Red
+            led14.BackColor = Color.Red
+            led18.BackColor = Color.Red
+            led19.BackColor = Color.Red
+            led22.BackColor = Color.Red
+            led23.BackColor = Color.Red
+            led24.BackColor = Color.Red
+            led25.BackColor = Color.Red
+
+        End If
+
+        If counter = 2 Then
+            offLights()
+            led2.BackColor = Color.Red
+            led3.BackColor = Color.Red
+            led4.BackColor = Color.Red
+            led6.BackColor = Color.Red
+            led9.BackColor = Color.Red
+            led10.BackColor = Color.Red
+            led13.BackColor = Color.Red
+            led14.BackColor = Color.Red
+            led17.BackColor = Color.Red
+            led18.BackColor = Color.Red
+            led21.BackColor = Color.Red
+            led22.BackColor = Color.Red
+            led23.BackColor = Color.Red
+            led24.BackColor = Color.Red
+            led25.BackColor = Color.Red
+        End If
+
+        If counter = 3 Then
+            offLights()
+            led1.BackColor = Color.Red
+            led2.BackColor = Color.Red
+            led3.BackColor = Color.Red
+            led4.BackColor = Color.Red
+            led9.BackColor = Color.Red
+            led10.BackColor = Color.Red
+            led12.BackColor = Color.Red
+            led13.BackColor = Color.Red
+            led14.BackColor = Color.Red
+            led19.BackColor = Color.Red
+            led20.BackColor = Color.Red
+            led21.BackColor = Color.Red
+            led22.BackColor = Color.Red
+            led23.BackColor = Color.Red
+            led24.BackColor = Color.Red
+        End If
+
+        If counter = 4 Then
+            offLights()
+            led1.BackColor = Color.Red
+            led2.BackColor = Color.Red
+            led4.BackColor = Color.Red
+            led5.BackColor = Color.Red
+            led6.BackColor = Color.Red
+            led7.BackColor = Color.Red
+            led9.BackColor = Color.Red
+            led10.BackColor = Color.Red
+            led12.BackColor = Color.Red
+            led13.BackColor = Color.Red
+            led14.BackColor = Color.Red
+            led15.BackColor = Color.Red
+            led19.BackColor = Color.Red
+            led20.BackColor = Color.Red
+            led24.BackColor = Color.Red
+            led25.BackColor = Color.Red
+        End If
+
+        If counter = 5 Then
+            offLights()
+            led1.BackColor = Color.Red
+            led2.BackColor = Color.Red
+            led3.BackColor = Color.Red
+            led4.BackColor = Color.Red
+            led5.BackColor = Color.Red
+            led6.BackColor = Color.Red
+            led7.BackColor = Color.Red
+            led11.BackColor = Color.Red
+            led12.BackColor = Color.Red
+            led13.BackColor = Color.Red
+            led14.BackColor = Color.Red
+            led15.BackColor = Color.Red
+            led19.BackColor = Color.Red
+            led20.BackColor = Color.Red
+            led21.BackColor = Color.Red
+            led22.BackColor = Color.Red
+            led23.BackColor = Color.Red
+            led24.BackColor = Color.Red
+            led25.BackColor = Color.Red
+        End If
+
+        If counter = 6 Then
+            offLights()
+            led1.BackColor = Color.Red
+            led2.BackColor = Color.Red
+            led3.BackColor = Color.Red
+            led4.BackColor = Color.Red
+            led5.BackColor = Color.Red
+            led6.BackColor = Color.Red
+            led7.BackColor = Color.Red
+            led11.BackColor = Color.Red
+            led12.BackColor = Color.Red
+            led13.BackColor = Color.Red
+            led14.BackColor = Color.Red
+            led15.BackColor = Color.Red
+            led16.BackColor = Color.Red
+            led17.BackColor = Color.Red
+            led20.BackColor = Color.Red
+            led21.BackColor = Color.Red
+            led22.BackColor = Color.Red
+            led23.BackColor = Color.Red
+            led24.BackColor = Color.Red
+            led25.BackColor = Color.Red
+        End If
+
+        If counter = 7 Then
+            offLights()
+            led1.BackColor = Color.Red
+            led2.BackColor = Color.Red
+            led3.BackColor = Color.Red
+            led4.BackColor = Color.Red
+            led5.BackColor = Color.Red
+            led9.BackColor = Color.Red
+            led10.BackColor = Color.Red
+            led14.BackColor = Color.Red
+            led15.BackColor = Color.Red
+            led19.BackColor = Color.Red
+            led20.BackColor = Color.Red
+            led24.BackColor = Color.Red
+            led25.BackColor = Color.Red
+        End If
+
+        If counter = 8 Then
+            offLights()
+            led2.BackColor = Color.Red
+            led3.BackColor = Color.Red
+            led4.BackColor = Color.Red
+            led6.BackColor = Color.Red
+            led7.BackColor = Color.Red
+            led9.BackColor = Color.Red
+            led10.BackColor = Color.Red
+            led12.BackColor = Color.Red
+            led13.BackColor = Color.Red
+            led14.BackColor = Color.Red
+            led16.BackColor = Color.Red
+            led17.BackColor = Color.Red
+            led19.BackColor = Color.Red
+            led20.BackColor = Color.Red
+            led22.BackColor = Color.Red
+            led23.BackColor = Color.Red
+            led24.BackColor = Color.Red
+        End If
+
+        If counter = 9 Then
+            offLights()
+            led2.BackColor = Color.Red
+            led3.BackColor = Color.Red
+            led4.BackColor = Color.Red
+            led6.BackColor = Color.Red
+            led7.BackColor = Color.Red
+            led9.BackColor = Color.Red
+            led10.BackColor = Color.Red
+            led12.BackColor = Color.Red
+            led13.BackColor = Color.Red
+            led14.BackColor = Color.Red
+            led15.BackColor = Color.Red
+            led19.BackColor = Color.Red
+            led20.BackColor = Color.Red
+            led22.BackColor = Color.Red
+            led23.BackColor = Color.Red
+            led24.BackColor = Color.Red
+        End If
+
+
+    End Sub
+
+    Private Sub stopbutton_Click(sender As Object, e As EventArgs) Handles stopbutton.Click
+        Timer.Stop()
+
+    End Sub
+
+    Private Sub contbuttn_Click(sender As Object, e As EventArgs) Handles contbuttn.Click
+        Timer.Start()
+
+    End Sub
+End Class
